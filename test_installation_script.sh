ffd9f0594c018d7f537bae62b46daafbeb781c50
jonathan.l <jonathan.l@hdcorp.co.uk>
2024-04-12 21:32:43 +0100

    updated installation script and inventory file

--- test_installation_script.sh
+++ test_installation_script.sh
@@ -17,6 +17,27 @@ if ! command_exists docker-compose; then
     exit 1
 fi
 
+# Install dependencies to use repositories over HTTPS
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Add Docker's official GPG key for Raspberry Pi
+curl -fsSL https://download.docker.com/linux/raspbian/gpg | sudo apt-key add -
+
+# Add Docker repository for Raspberry Pi
+echo "deb [arch=armhf] https://download.docker.com/linux/raspbian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+
+# Update package lists again to include Docker repository
+sudo apt update -y
+
+# Install Docker Community Edition
+sudo apt install -y docker-ce
+
+# Add current user to the docker group to run Docker without sudo
+sudo usermod -aG docker $USER
+
+# Install Ansible
+sudo apt install -y ansible
+
 # Run Docker Compose YAML files
 docker-compose -f docker-composers/mysql-server.yml \
                -f docker-composers/zabbix-java-gateway.yml \

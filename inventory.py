35610d20becd435a1fa853908454323e49ad14ba
jonathan.l <jonathan.l@hdcorp.co.uk>
2024-04-15 20:19:22 +0100

    updated inventory file, trying json format.

---
+++ inventory.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import json
+
+inventory = {
+    "all": {
+        "hosts": {
+            "pinas": {"ansible_host": "192.168.1.134"},
+            "new1gb": {"ansible_host": "192.168.1.14"},
+            "new2gb": {"ansible_host": "192.168.1.16"},
+            "new3": {"ansible_host": "192.168.1.17"}
+        }
+    }
+}
+
+try:
+    print(json.dumps(inventory))
+except Exception as e:
+    print(f"Error: {e}")
+    exit(1)
